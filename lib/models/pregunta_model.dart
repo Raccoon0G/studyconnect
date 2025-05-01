182ee9e7f617abc5502f7ce807e1f8d825de840c
Jeovanny <jeovannytmartinez@gmail.com>
2025-05-01 10:41:39 -0600

    solucione un error en mi pregunta model

--- lib/models/pregunta_model.dart
+++ lib/models/pregunta_model.dart
@@ -2,18 +2,21 @@ class Pregunta {
   final String pregunta;
   final Map<String, String> opciones;
   final String respuestaCorrecta;
+  final String dificultad; // Asegúrate de que esto esté aquí
 
   Pregunta({
     required this.pregunta,
     required this.opciones,
     required this.respuestaCorrecta,
+    required this.dificultad, // Y aquí también
   });
 
   factory Pregunta.fromJson(Map<String, dynamic> json) {
     return Pregunta(
-      pregunta: json["pregunta"],
-      opciones: Map<String, String>.from(json["opciones"]),
-      respuestaCorrecta: json["respuesta_correcta"],
+      pregunta: json['pregunta'],
+      opciones: Map<String, String>.from(json['opciones']),
+      respuestaCorrecta: json['respuestaCorrecta'],
+      dificultad: json['dificultad'], // importante para Make
     );
   }
 }
